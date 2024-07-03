9cc3b9c494c19d8079ed1c36e1a695d45b3cf7a3
Suaste Cherizola <jsuaste86@gmail.com>
2024-07-03 11:27:07 +0200

    email logic

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,155 +1,186 @@
-import { useState } from "react";
-import "../styles/CallToAction.css";
-import { toast, ToastContainer } from 'react-toastify';
-import { TextField, Button, Container, Typography} from '@mui/material';
-import 'react-toastify/dist/ReactToastify.css';
+import { useRef, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { TextField, Button, Container, Typography } from "@mui/material";
+import "react-toastify/dist/ReactToastify.css";
+import emailjs from "@emailjs/browser";
 
 export default function CallToAction() {
-  
+  const formRef = useRef();
+  const [isLoading, setIsLoading] = useState(false)
+
   const [formData, setFormData] = useState({
-      name: '',
-      email: '',
-      content: ''
-    });
-  
+    name: "",
+    email: "",
+    content: "",
+  });
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
-    ...formData,
-    [name]: value
+      ...formData,
+      [name]: value,
     });
-  }; 
-  
+  };
+
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault();    
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (! re.test(String(formData.email).toLowerCase()) ) {
-      toast.error('Invalid email address', {
-        className: 'custom-toast',
-        bodyClassName: 'custom-toast-container'
-        }
-      );
+    if (!re.test(String(formData.email).toLowerCase())) {
+      toast.error("Invalid email address", {
+        className: "custom-toast",
+        bodyClassName: "custom-toast-container",
+        autoClose: 2000,
+      });
       return;
     }
+    setIsLoading(true);
+    emailjs.sendForm("service_zlzok61", "template_mdga1xg", formRef.current, {
+        publicKey: "hnPNkDjQXdQhSTL0s",
+      })
+      .then(() => {
+        toast.success(
+          "Thank you for reaching out. I will get in touch with you as soon as possible",
+          {
+            className: "custom-toast",
+            bodyClassName: "custom-toast-container",
+            autoClose: 3000,
+            isClosable: true,
+            position: "top-center",
+          }
+        );
+        setIsLoading(false)
+        setFormData({
+          name: "",
+          email: "",
+          content: "",
+        });
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        toast.error("Internal server error. Please try again later", {
+          className: "custom-toast",
+          bodyClassName: "custom-toast-container",
+          autoClose: 2000,
+        });
+      });
   };
 
   return (
-  <div className="contact-container">
-          Any idea to colaborate? Looking forward to hear from you:
+    <div className="contact-container">
 
-    <Container maxWidth="sm">
-      <Typography variant="h3 " component="h3" sx={{mb:1}} gutterBottom>
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <div style={{display:"flex"}}>
-        <TextField
-      fullWidth
-      margin="normal"
-      label="Name"
-      name="name"
-      value={formData.name}
-      onChange={handleChange}
-      required
-      sx={{
-        '& .MuiInputBase-root': {
-          color: 'white', 
-        },
-        '& .MuiOutlinedInput-root': {
-          '& fieldset': {
-            borderColor: 'white',  
-          },
-          '&:hover fieldset': {
-            borderColor: 'primary.light', 
-          },
-         
-        },
-        '& .MuiInputLabel-root': {
-          color: 'white',  
-        },
-        '& .MuiInputLabel-root.Mui-focused': {
-          color: 'white',  
-        },
-      }}
-    />
-        <TextField
-          fullWidth
-          margin="normal"
-          label="Email"
-          name="email"
-          type="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-          sx={{
-            ml:"10px",
-            '& .MuiInputBase-root': {
-              color: 'white',  
-            },
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                borderColor: 'white',  
+    {isLoading && 
+      <div className="loading-container">
+        <span className="loading-effect"></span>
+      </div>
+    }
+
+    <Container maxWidth="sm" className={isLoading? "disabled-box":""}>
+      Any idea to colaborate? Looking forward to hear from you:
+
+        <Typography
+          variant="h3 "
+          component="h3"
+          sx={{ mb: 1 }}
+          gutterBottom
+        ></Typography>
+        <form ref={formRef} onSubmit={handleSubmit}>
+          <div style={{ display: "flex" }}>
+            <TextField
+              fullWidth
+              margin="normal"
+              label="Name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              sx={{
+                "& .MuiInputBase-root": {
+                  color: "white",
+                },
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderColor: "white",
+                  },
+                  "&:hover fieldset": {
+                    borderColor: "primary.light",
+                  },
+                },
+                "& .MuiInputLabel-root": {
+                  color: "white",
+                },
+                "& .MuiInputLabel-root.Mui-focused": {
+                  color: "white",
+                },
+              }}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              label="Email"
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              sx={{
+                ml: "10px",
+                "& .MuiInputBase-root": {
+                  color: "white",
+                },
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderColor: "white",
+                  },
+                  "&:hover fieldset": {
+                    borderColor: "primary.light",
+                  },
+                },
+                "& .MuiInputLabel-root": {
+                  color: "white",
+                },
+                "& .MuiInputLabel-root.Mui-focused": {
+                  color: "white",
+                },
+              }}
+            />
+          </div>
+
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Content"
+            name="content"
+            multiline
+            rows={4}
+            value={formData.content}
+            onChange={handleChange}
+            required
+            sx={{
+              "& .MuiInputBase-root": {
+                color: "white",
               },
-              '&:hover fieldset': {
-                borderColor: 'primary.light', 
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "white",
+                },
+                "&:hover fieldset": {
+                  borderColor: "primary.light",
+                },
               },
-             
-            },
-            '& .MuiInputLabel-root': {
-              color: 'white',  
-            },
-            '& .MuiInputLabel-root.Mui-focused': {
-              color: 'white',
-            },
-          }}
-         
+              "& .MuiInputLabel-root": {
+                color: "white",
+              },
+              "& .MuiInputLabel-root.Mui-focused": {
+                color: "white",
+              },
+            }}
           />
-        </div>
-
-        <TextField
-          fullWidth
-          margin="normal"
-          label="Content"
-          name="content"
-          multiline
-          rows={4}
-          value={formData.content}
-          onChange={handleChange}
-          required
-          sx={{
-            '& .MuiInputBase-root': {
-              color: 'white',  
-            },
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                borderColor: 'white', 
-              },
-              '&:hover fieldset': {
-                borderColor: 'primary.light', 
-              },
-             
-            },
-            '& .MuiInputLabel-root': {
-              color: 'white', 
-            },
-            '& .MuiInputLabel-root.Mui-focused': {
-              color: 'white', 
-            },
-          }}
-        />
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-          >
+          <Button variant="contained" color="primary" type="submit">
             Submit
           </Button>
-      </form>
-    </Container>
-    
-    <ToastContainer />
-
-  </div>
-  )
+        </form>
+      </Container>
+      <ToastContainer />
+    </div>
+  );
 }
-
- 
